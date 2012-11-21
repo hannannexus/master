76bd7c7be35561b6a6e02396b612021fee5f3f96
Ilya <mephis@bk.ru>
2012-11-21 13:04:58 +0200

    tooltips v1

--- public/js/main.js
+++ public/js/main.js
@@ -205,24 +205,69 @@ function drawChart(result) {
 		coords.push(new google.maps.LatLng(result[i].lat, result[i].lan));
 		distance = google.maps.geometry.spherical.computeLength(coords);
 		altitude_chart.push([distance/1000, result[i].alt]);
-		speed_chart.push([distance/1000, result[i].speed*10]);
+		speed_chart.push([distance/1000, result[i].speed*5]);
 	}
 	
-    $.plot($("#chart_canvas"), [{ data: altitude_chart}, {data: speed_chart}], {
+    plot = $.plot($("#chart_canvas"), [{ data: altitude_chart}, {data: speed_chart}], {
     	lines: { show: true, fill: true },
         crosshair: { mode: "x" },
-        grid: { hoverable: true, autoHighlight: false },
-        tooltip:        true,       //false
-        tooltipOpts: {
-          content:      "X: %x.2 | Y: %y.2",
-          shifts: { 
-            x:          10,
-            y:          50
-          },
-          defaultTheme: true
-        }
+        grid: { hoverable: true, autoHighlight: false }
     });
 }
+
+$(function () {
+	
+	var updateLegendTimeout = null;
+    var latestPosition = null;
+    
+    function updateLegend() {
+        updateLegendTimeout = null;
+        
+        var pos = latestPosition;
+        
+        var axes = plot.getAxes();
+        if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+            pos.y < axes.yaxis.min || pos.y > axes.yaxis.max)
+            return;
+
+        var i, j, dataset = plot.getData();
+        index_data = [];
+        
+        for (i = 0; i < dataset.length; ++i) {
+            var series = dataset[i];
+            
+            // find the nearest points, x-wise
+            for (j = 0; j < series.data.length; ++j)
+                if (series.data[j][0] > pos.x)
+                    break;
+            
+            // now interpolate
+            var y, p1 = series.data[j - 1], p2 = series.data[j];
+            if (p1 == null)
+                y = p2[1];
+            else if (p2 == null)
+                y = p1[1];
+            else
+                y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+            index_data[i] = y;
+        }
+    }
+    
+    $('#chart_canvas').mousemove(function(e){
+		$('#info').remove();
+		$('body').append('<div id="info" style="position: absolute;"></div>');
+		$('#info').html('alt: ' + floorNumber(index_data[0],2) + ' spd: ' + floorNumber(index_data[1]/5, 2));
+		$('#info').css('left', e.clientX);
+		$('#info').css('top', e.clientY+200);
+	});
+    
+    $("#chart_canvas").bind("plothover",  function (event, pos, item) {
+        latestPosition = pos;
+        if (!updateLegendTimeout)
+            updateLegendTimeout = setTimeout(updateLegend, 50);
+    });
+})
+
 
 
 
